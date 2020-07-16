de0355fdcf941cf24908f1c5dbd0e7d4e728ddf1
Anton Ekblad <anton@ekblad.cc>
2020-07-16 10:42:26 +0200

    Use string splitting instead of regex to format UUIDs.

--- users-to-json.sh
+++ users-to-json.sh
@@ -3,19 +3,17 @@ users_left=$#
 user_lines=""
 
 format_uuid() {
-  u="[a-fA-F0-9]"
-  regex="\($u\{8\}\)\($u\{4\}\)\($u\{4\}\)\($u\{4\}\)\($u\{12\}\)"
-  echo $1 | sed -e "s/$regex/\1-\2-\3-\4-\5/g"
+  echo ${1:0:8}-${1:8:4}-${1:12:4}-${1:16:4}-${1:20}
 }
 
 for user in $@ ; do
   let users_left=$users_left-1
   profile=`curl -Ls https://api.mojang.com/users/profiles/minecraft/$user`
-  raw_uuid=`echo $profile | jq '.id'`
+  raw_uuid=`echo $profile | jq -j '.id'`
 
   if [ "$raw_uuid" != "" ] ; then
     uuid=`format_uuid $raw_uuid`
-    user_lines="{\"uuid\": $uuid, \"name\": \"$user\"}\n$user_lines"
+    user_lines="{\"uuid\": \"$uuid\", \"name\": \"$user\"}\n$user_lines"
   fi
 done
 
